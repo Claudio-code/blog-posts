98181a273719bfe9e70df234c6ecb7fd086b9b62
claudio-code <suporte2424@outlook.com>
2020-07-20 00:20:15 -0300

    baixado o profiler para debuger

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use \Symfony\Component\Routing\Annotation\Route;
 use \Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * Class DefaultController
@@ -20,6 +20,27 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render("index.html.twig");
+        $posts = [
+            [
+                "id" => 1,
+                "title" => "post 1",
+                "create_at" => "2020-02-10 13:43:04"
+            ],
+            [
+                "id" => 2,
+                "title" => "post 2",
+                "create_at" => "2004-04-04 15:23:04"
+            ],
+            [
+                "id" => 3,
+                "title" => "post 3",
+                "create_at" => "2024-04-10 11:21:14"
+            ]
+        ];
+
+        return $this->render("index.html.twig", [
+            "title" => "Home page",
+            "posts" => $posts
+        ]);
     }
 }
