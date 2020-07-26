b871ea49476d2dd92044cfc4dbb8a743b62f643d
claudio-code <suporte2424@outlook.com>
2020-07-26 07:26:28 -0300

    integrado com bootstrap

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use \Symfony\Component\HttpFoundation\Response;
@@ -14,66 +16,82 @@ use \Symfony\Component\HttpFoundation\Request;
 class PostController extends AbstractController
 {
     /**
+     * @return Response
      * @Route("/", name="_index", methods={"GET"})
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
-        return $this->render("post/index.html.twig");
+        return $this->render("post/index.html.twig", [
+            "posts" => $postRepository->findAll()
+        ]);
     }
 
     /**
+     * @return Response
+     * @Route("/remove/{id}", name="_remove", methods={"GET", "POST"})
+     */
+    public function remove(Post $post): Response
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($post);
+        $manager->flush();
+        $this->addFlash("errors", "deletado com sucesso.");
+
+        return $this->redirectToRoute("user_index");
+    }
+
+    /**
+     * @return Response
      * @Route("/edit/{id}", name="_edit", methods={"GET", "POST"})
      */
     public function edit(Request $request, Post $post)
     {
-        $data = $request->request->all();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
 
-        if (empty($data)) {
-            return $this->render(
-                "post/edit.html.twig", [ "post" => $post ]
-            );
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setUpdatedAt(
+                new \DateTime("now", new \DateTimeZone("America/Sao_Paulo")))
+            ;
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($post);
+            $manager->flush();
+            $this->addFlash("success", "alterado novo post");
+
+            return  $this->redirectToRoute("post_index");
         }
 
-        $post
-            ->setTitle($data["title"])
-            ->setUpdatedAt(new \DateTime("now", new \DateTimeZone("America/Sao_Paulo")))
-            ->setDescription($data["description"])
-            ->setSlug($data["slug"])
-            ->setContent($data["content"])
-        ;
-
-        $doctrine = $this->getDoctrine()->getManager();
-        $doctrine->persist($post);
-        $doctrine->flush();
-
-        return $this->render("post/index.html.twig");
+        return $this->render("post/create.html.twig", [
+            "form" => $form->createView()
+        ]);
     }
 
     /**
+     * @return Response
      * @Route("/create", name="_create", methods={"GET", "POST"})
      */
     public function create(Request $request): Response
     {
-        $data = $request->request->all();
+        $form = $this->createForm(PostType::class, new Post());
+        $form->handleRequest($request);
 
-        if (empty($data)) {
-            return $this->render("post/create.html.twig");
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post
+                ->setCreatedAt(new \DateTime("now", new \DateTimeZone("America/Sao_Paulo")))
+                ->setUpdatedAt(new \DateTime("now", new \DateTimeZone("America/Sao_Paulo")))
+            ;
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($post);
+            $manager->flush();
+            $this->addFlash("success", "criado novo post");
+
+            return  $this->redirectToRoute("post_index");
         }
 
-        $post = new Post();
-        $post
-            ->setTitle($data["title"])
-            ->setCreatedAt(new \DateTime("now", new \DateTimeZone("America/Sao_Paulo")))
-            ->setUpdatedAt(new \DateTime("now", new \DateTimeZone("America/Sao_Paulo")))
-            ->setDescription($data["description"])
-            ->setSlug($data["slug"])
-            ->setContent($data["content"])
-        ;
-
-        $doctrine = $this->getDoctrine()->getManager();
-        $doctrine->persist($post);
-        $doctrine->flush();
-
-        return $this->render("post/index.html.twig");
+        return $this->render("post/create.html.twig", [
+            "form" => $form->createView()
+        ]);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\UserRepository;
 use \Symfony\Component\HttpFoundation\Request;
 use \Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,11 +16,64 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     /**
+     * @return Response
      * @Route("/", name="_index", methods={"GET", "POST"})
      */
-    public function index(Request $request): Response
+    public function index(UserRepository $repository): Response
     {
+        return $this->render("user/list.html.twig", [
+            "users" => $repository->findAll()
+        ]);
+    }
 
+    /**
+     * @return Response
+     * @Route("/remove/{id}", name="_remove", methods={"GET", "POST"})
+     */
+    public function remove(User $user): Response
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($user);
+        $manager->flush();
+        $this->addFlash("errors", "deletado com sucesso.");
+
+        return $this->redirectToRoute("user_index");
+    }
+
+    /**
+     * @return Response
+     * @Route("/edit/{id}", name="_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, User $user): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $user->setUpdatedAt(
+                new \DateTime("now", new \DateTimeZone("America/Sao_Paulo"))
+            );
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user);
+            $manager->flush();
+            $this->addFlash("success", "editado novo usuario");
+
+            return  $this->redirectToRoute("user_index");
+        }
+
+        return $this->render("user/index.html.twig", [
+            "form" => $form->createView()
+        ]);
+    }
+
+    /**
+     * @return Response
+     * @Route("/create", name="_create", methods={"GET", "POST"})
+     */
+    public function create(Request $request): Response
+    {
         $form = $this->createForm(UserType::class, new User());
         $form->handleRequest($request);
 
@@ -38,8 +92,8 @@ class UserController extends AbstractController
             return  $this->redirectToRoute("user_index");
         }
 
-        return $this->render('user/index.html.twig', [
-            'form' => $form->createView()
+        return $this->render("user/index.html.twig", [
+            "form" => $form->createView()
         ]);
     }
 }
